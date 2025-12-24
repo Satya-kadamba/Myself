fbea2ae268d273045c4ae94e4a8a0dbd702f0352
satya-kadamba <kadambasatyabhaskar@gmail.com>
2025-12-24 11:18:40 +0530

    html added

---
+++ HTML/hello.js
@@ -0,0 +1,28 @@
+console.log("this is a log message");
+console.warn("this is a warning");
+console.error("This is an error");
+console.info("this is info");
+console.log('name:',"john",'age:',25);
+console.log({name:'king',age:22});
+console.log({name:'satya',age:'22'},
+            {name:'satishu',role:'developer'
+            });
+
+alert('Be alert, your on camera !')
+let userName=prompt('Mention your name');
+console.log('user entered:',userName);
+let isComfirmed = confirm('Do you want to proceed');
+
+console.log("======================================");
+
+let num1=prompt('Enter the first number');
+let num2=prompt('enter the second number');
+
+num1= Number(num1);
+num2= Number(num2);
+
+let sum = num1 + num2;
+let difference = num1 - num2;
+let product = num1 * num2;
+let quotient = num1 / num2;
+
